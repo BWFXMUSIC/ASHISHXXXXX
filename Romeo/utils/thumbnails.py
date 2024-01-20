6ef4d0cb20493c2d36696fc913161f65cb24f444
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-01-20 18:19:30 +0530

    Update thumbnails.py

--- Romeo/utils/thumbnails.py
+++ Romeo/utils/thumbnails.py
@@ -1,15 +1,17 @@
 import os
 import re
 import textwrap
-
+import random
 import aiofiles
 import aiohttp
 from PIL import (Image, ImageDraw, ImageEnhance, ImageFilter,
                  ImageFont, ImageOps)
 from youtubesearchpython.__future__ import VideosSearch
 
-from Romeo import app
-from config import YOUTUBE_IMG_URL
+from config import MUSIC_BOT_NAME, YOUTUBE_IMG_URL
+
+themes = ["blue", "red", "pink", "purple"]
+colors = ["white", "black", "red", "orange", "yellow", "green", "cyan", "azure", "blue", "violet", "magenta", "pink"]
 
 
 def changeImageSize(maxWidth, maxHeight, image):
@@ -17,13 +19,15 @@ def changeImageSize(maxWidth, maxHeight, image):
     heightRatio = maxHeight / image.size[1]
     newWidth = int(widthRatio * image.size[0])
     newHeight = int(heightRatio * image.size[1])
-    return image.resize((newWidth, newHeight))
+    newImage = image.resize((newWidth, newHeight))
+    return newImage
 
 
-async def get_thumb(videoid):
-    if os.path.isfile(f"cache/{videoid}.png"):
-        return f"cache/{videoid}.png"
-
+async def gen_thumb(videoid, user_id):
+    try:
+        os.remove(f"cache/thumb{videoid}.png")
+    except:
+        pass
     url = f"https://www.youtube.com/watch?v={videoid}"
     try:
         results = VideosSearch(url, limit=1)
@@ -57,85 +61,99 @@ async def get_thumb(videoid):
                     await f.write(await resp.read())
                     await f.close()
 
-        youtube = Image.open(f"cache/thumb{videoid}.png")
-        image1 = changeImageSize(1280, 720, youtube)
-        image2 = image1.convert("RGBA")
-        background = image2.filter(filter=ImageFilter.BoxBlur(30))
-        enhancer = ImageEnhance.Brightness(background)
-        background = enhancer.enhance(0.6)
-        Xcenter = youtube.width / 2
-        Ycenter = youtube.height / 2
-        x1 = Xcenter - 250
-        y1 = Ycenter - 250
-        x2 = Xcenter + 250
-        y2 = Ycenter + 250
-        logo = youtube.crop((x1, y1, x2, y2))
-        logo.thumbnail((520, 520), Image.LANCZOS)
-        logo = ImageOps.expand(logo, border=15, fill="white")
-        background.paste(logo, (50, 100))
-        draw = ImageDraw.Draw(background)
-        font = ImageFont.truetype("assets/font2.ttf", 40)
-        font2 = ImageFont.truetype("assets/font2.ttf", 70)
-        arial = ImageFont.truetype("assets/font2.ttf", 30)
-        name_font = ImageFont.truetype("Romeo/assets/font.ttf", 30)
-        para = textwrap.wrap(title, width=32)
-        j = 0
-        draw.text(
-            (5, 5), f"{'Romeo'}", fill="white", font=name_font
-        )
-        draw.text(
-            (600, 150),
-            "STARTED PLAYING",
-            fill="white",
-            stroke_width=2,
-            stroke_fill="white",
-            font=font2,
-        )
-        for line in para:
-            if j == 1:
-                j += 1
-                draw.text(
-                    (600, 340),
-                    f"{line}",
-                    fill="white",
-                    stroke_width=1,
-                    stroke_fill="white",
-                    font=font,
-                )
-            if j == 0:
-                j += 1
-                draw.text(
-                    (600, 280),
-                    f"{line}",
-                    fill="white",
-                    stroke_width=1,
-                    stroke_fill="white",
-                    font=font,
-                )
-
-        draw.text(
-            (600, 450),
-            f"Views : {views[:23]}",
-            (255, 255, 255),
-            font=arial,
-        )
-        draw.text(
-            (600, 500),
-            f"Duration : {duration[:23]} Mins",
-            (255, 255, 255),
-            font=arial,
-        )
-        draw.text(
-            (600, 550),
-            f"Channel : {channel}",
-            (255, 255, 255),
-            font=arial,
-        )
+        images = random.choice(themes)
+        border = random.choice(colors)
+        image1 = Image.open(f"cache/thumb{videoid}.png")
+        image2 = Image.open(f"helper/rj/{images}.png")
+        image3 = changeImageSize(1280, 720, image1)
+        image4 = changeImageSize(1280, 720, image2)
+        image5 = image3.convert("RGBA")
+        image6 = image4.convert("RGBA")
+        Image.alpha_composite(image5, image6).save("cache/temp.png")
+        logo = Image.open("cache/temp.png")
+        img = ImageOps.expand(logo, border=10, fill=f"{border}")
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype("helper/rj/font.otf", 55)
+        font2 = ImageFont.truetype("helper/rj/font.otf", 35)
+        draw.text((670, 455), f"Title: {title[:50]} ...", (255, 255, 255), font=font)
+        draw.text((670, 450), f"Duration: {duration}", (255, 255, 255), font=font)
+        draw.text((670, 450), f"Views: {views}", (255, 255, 255), font=font)
+        draw.text((20, 20), f"L2R•MUSIC", (255, 255, 255), font=font2)
         try:
             os.remove(f"cache/thumb{videoid}.png")
+            os.remove(f"cache/temp.png")
         except:
             pass
-        background.save(f"cache/{videoid}.png")
+        img.save(f"cache/{videoid}.png")
+        return f"cache/{videoid}.png"
+    except Exception:
+        return YOUTUBE_IMG_URL
+        
+        
+        
+        
+async def gen_qthumb(videoid, user_id):
+    try:
+        os.remove(f"cache/thumb{videoid}.png")
+    except:
+        pass
+    url = f"https://www.youtube.com/watch?v={videoid}"
+    try:
+        results = VideosSearch(url, limit=1)
+        for result in (await results.next())["result"]:
+            try:
+                title = result["title"]
+                title = re.sub("\W+", " ", title)
+                title = title.title()
+            except:
+                title = "Unsupported Title"
+            try:
+                duration = result["duration"]
+            except:
+                duration = "Unknown Mins"
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+            try:
+                views = result["viewCount"]["short"]
+            except:
+                views = "Unknown Views"
+            try:
+                channel = result["channel"]["name"]
+            except:
+                channel = "Unknown Channel"
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(thumbnail) as resp:
+                if resp.status == 200:
+                    f = await aiofiles.open(
+                        f"cache/thumb{videoid}.png", mode="wb"
+                    )
+                    await f.write(await resp.read())
+                    await f.close()
+
+        images = random.choice(themes)
+        border = random.choice(colors)
+        image1 = Image.open(f"cache/thumb{videoid}.png")
+        image2 = Image.open(f"helper/rj/{images}.png")
+        image3 = changeImageSize(1280, 720, image1)
+        image4 = changeImageSize(1280, 720, image2)
+        image5 = image3.convert("RGBA")
+        image6 = image4.convert("RGBA")
+        Image.alpha_composite(image5, image6).save("cache/temp.png")
+        logo = Image.open("cache/temp.png")
+        img = ImageOps.expand(logo, border=10, fill=f"{border}")
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype("helper/rj/font.otf", 55)
+        font2 = ImageFont.truetype("helper/rj/font.otf", 35)
+        draw.text((670, 445), f"Title: {title[:50]} ...", (255, 255, 255), font=font)
+        draw.text((670, 450), f"Duration: {duration}", (255, 255, 255), font=font)
+        draw.text((670, 450), f" Views: {views}", (255, 255, 255), font=font)
+        draw.text((20, 20), f"L2R MUSIC", (255, 255, 255), font=font2)
+        try:
+            os.remove(f"cache/thumb{videoid}.png")
+            os.remove(f"cache/temp.png")
+        except:
+            pass
+        img.save(f"cache/{videoid}.png")
         return f"cache/{videoid}.png"
     except Exception:
         return YOUTUBE_IMG_URL
