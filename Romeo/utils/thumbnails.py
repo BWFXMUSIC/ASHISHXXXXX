772871105a3402c7f265a78c5b70ab16f79887fa
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-01-20 14:01:36 +0530

    Update thumbnails.py

--- Romeo/utils/thumbnails.py
+++ Romeo/utils/thumbnails.py
@@ -38,7 +38,7 @@ def truncate(text):
     text2 = text2.strip()     
     return [text1,text2]
 
-async def gen_thumb(videoid):
+async def get_thumb(videoid):
     try:
         if os.path.isfile(f"cache/{videoid}.jpg"):
             return f"cache/{videoid}.jpg"
@@ -84,7 +84,7 @@ async def gen_thumb(videoid):
             background = enhancer.enhance(0.6)
             image2 = background
                                                                                             
-            circle = Image.open("assets/circle.png")
+            circle = Image.open("AshishX/assets/circle.png")
 
             # changing circle color
             im = circle
@@ -116,13 +116,13 @@ async def gen_thumb(videoid):
             image2.paste(circle, (0,0), mask = circle)
 
             # fonts
-            font1 = ImageFont.truetype('assets/font.ttf', 30)
-            font2 = ImageFont.truetype('assets/font2.ttf', 70)
-            font3 = ImageFont.truetype('assets/font2.ttf', 40)
-            font4 = ImageFont.truetype('assets/font2.ttf', 35)
+            font1 = ImageFont.truetype('AshishX/assets/font.ttf', 30)
+            font2 = ImageFont.truetype('AshishX/assets/font2.ttf', 70)
+            font3 = ImageFont.truetype('AshishX/assets/font2.ttf', 40)
+            font4 = ImageFont.truetype('AshishX/assets/font2.ttf', 35)
 
             image4 = ImageDraw.Draw(image2)
-            image4.text((10, 10), "L2R MUSIC", fill="white", font = font1, align ="left") 
+            image4.text((10, 10), "ASHISH MUSIC", fill="white", font = font1, align ="left") 
             image4.text((670, 150), "NOW PLAYING", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
 
             # title
